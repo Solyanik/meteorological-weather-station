0f807f5d77b6b90832092f5470faee4ee36ac914
Solyanik <obslpk@yandex.ru>
2017-08-08 22:05:53 +0200

    Fixed grafix errors and connected library tether.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -8,7 +8,7 @@ angular.module('index', []).controller('main', ['$scope', '$http', function($sco
 	$scope.width = 1140;
     $scope.height = 500;
     $scope.yAxis = 'mm, tmax, tmin, af, rain, sun';
-    $scope.xAxis = '';
+    $scope.xAxis = 'time';
     $scope.list_mws = [];
     $scope.line_tmax = true;
     $scope.line_tmin = false;
@@ -39,17 +39,30 @@ angular.module('index', []).controller('main', ['$scope', '$http', function($sco
 	}
 
 	//Graph
+	$scope.set_params = function (index, id_name, max, column, fData) {
+		
+		//$("#line-" + index).attr("y1", (($scope.data.search ? $scope.data.search[index - 1][3] : $scope.list_mws[index - 1][3]) / $scope.max_tmin * $scope.height/2));
+		x1 = (index / (fData ? fData.length : $scope.list_mws.length) * $scope.width);
+		y1 = index ? ((fData ? fData[index - 1][column] : $scope.list_mws[index - 1][column]) / max * $scope.height/2) : 0
+		x2 = (index + 1) / (fData ? fData.length : $scope.list_mws.length) * $scope.width;
+		y2 = (fData ? fData[index][column] : $scope.list_mws[index][column]) / max * $scope.height/2;
+
+		$(id_name + index).attr("x1", x1);
+		$(id_name + index).attr("y1", (isNaN(y1) ? 0 : y1) );	
+		$(id_name + index).attr("x2", x2);
+		$(id_name + index).attr("y2", (isNaN(y2) ? 0 : y2) );
+
+		return true;
+	}
     // Data 
     $scope.set_list_mws = function (data) {
 		$scope.list_mws = data;
 		
 		// Find Maximum X & Y Axis Values - this is used to position the data as a percentage of the maximum
-	    $scope.max_mm = 0;
 	    $scope.max_tmax = 0;
 	    $scope.max_tmin = 0;
 	    $scope.max_af = 0;
 	    $scope.max_rain = 0;
-	    $scope.max_sun = 0;
 	    
 	    var arrLength = $scope.list_mws.length;
 	    for (var i = 0; i < arrLength; i++) {
